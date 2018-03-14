54c80f05728c071ff421f2bab5b333d235f12e48
Chandler <chandlermdavidson@gmail.com>
2018-03-14 13:34:05 -0500

    Fixed Visual Studio's Dumb Spaces

--- CoffeeOrderingSystem/DBConnection.cs
+++ CoffeeOrderingSystem/DBConnection.cs
@@ -4,33 +4,33 @@ using MySql.Data.MySqlClient;
 
 namespace CoffeeOrderingSystem
 {
-    public class DBConnection
-    {
-        private static readonly DBConnection _instance = new DBConnection();
-        public static DBConnection Instance { get { return _instance; } }
-        private static MySqlConnection sqlConnection;
-        private string databaseName = string.Empty;
+	public class DBConnection
+	{
+		private static readonly DBConnection _instance = new DBConnection();
+		public static DBConnection Instance { get { return _instance; } }
+		private static MySqlConnection sqlConnection;
+		private string databaseName = string.Empty;
 
-        DBConnection()
-        {
-            try
-            {
-                string connDetails = string.Format("Server=localhost; database={0}; UID=UserName; password=your password", databaseName);
-                sqlConnection = new MySqlConnection(connDetails);
-                sqlConnection.Open();
-            }
-            catch(MySqlException e)
-            {
-                Console.WriteLine("{0}: Error establishing connection.", e);
-            }
+		DBConnection()
+		{
+			try
+			{
+				string connDetails = string.Format("Server=localhost; database={0}; UID=UserName; password=your password", databaseName);
+				sqlConnection = new MySqlConnection(connDetails);
+				sqlConnection.Open();
+			}
+			catch(MySqlException e)
+			{
+				Console.WriteLine("{0}: Error establishing connection.", e);
+			}
 		}
 
-        public void Open() => sqlConnection.Open();
-        public void Close() => sqlConnection.Close();
+		public void Open() => sqlConnection.Open();
+		public void Close() => sqlConnection.Close();
 
-        public void AddOrder()
-        {
-            
-        }
-    }
+		public void AddOrder()
+		{
+			
+		}
+	}
 }
--- CoffeeOrderingSystem/src/BillingSystem.cs
+++ CoffeeOrderingSystem/src/BillingSystem.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 
 namespace CoffeeOrderingSystem.src
 {
-    public static class BillingSystem
-    {
-        public static HashSet<string> customerIDs = new HashSet<string>();
-        private static Random random = new Random();
+	public static class BillingSystem
+	{
+		public static HashSet<string> customerIDs = new HashSet<string>();
+		private static Random random = new Random();
 
-        public static string NewID()
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		public static string NewID()
+		{
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-            // Generate random string until a new one is found
-            for (int i = 0; i < Int32.MaxValue; i++) // Time out just in case
-            {
-                string id = "";
+			// Generate random string until a new one is found
+			for (int i = 0; i < Int32.MaxValue; i++) // Time out just in case
+			{
+				string id = "";
 
-                for (int j = 0; j < 8; j++)
-                    id += chars[random.Next(chars.Length)];
+				for (int j = 0; j < 8; j++)
+					id += chars[random.Next(chars.Length)];
 
-                if (!customerIDs.Contains(id))
-                    return id;
-            }
+				if (!customerIDs.Contains(id))
+					return id;
+			}
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 }
